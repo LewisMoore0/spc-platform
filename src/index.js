297a53c2cb7b486bf82cfa838606d1dbbd1f9b26
Lewis Moore <lewismooredev@gmail.com>
2023-03-07 19:57:14 +0000

    first ts function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 "use strict";
+let stringFunction = (string) => {
+    return [string];
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+"use strict";
+
+let stringFunction = (string: string): string[] => {
+    return [string]
+}
