552968a386a864b3afecc7611f3d2908ceaf30fa
Lewis Moore <lewismooredev@gmail.com>
2023-03-07 20:50:59 +0000

    Build

---
+++ src/addDogToDayCare.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addDogToDayCare = void 0;
+let addDogToDayCare = (dog) => {
+    return [
+        dog
+    ];
+};
+exports.addDogToDayCare = addDogToDayCare;
