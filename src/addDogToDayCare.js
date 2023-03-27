0daade03aca9ce4e0e0f6664cba8b2ab9d5c240f
Lewis Moore <lewismooredev@gmail.com>
2023-03-27 10:01:37 +0100

    Add .js files to git ignore

--- src/addDogToDayCare.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addDogToDayCare = void 0;
-let addDogToDayCare = (dog) => {
-    return [
-        dog
-    ];
-};
-exports.addDogToDayCare = addDogToDayCare;
