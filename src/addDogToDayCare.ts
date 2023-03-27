cefc5b0e3efab0441feb14d82a5290f3cf6c5f92
Lewis Moore <lewismooredev@gmail.com>
2023-03-27 12:17:47 +0100

    daycareArray can only hold 10 elements

--- src/addDogToDayCare.ts
+++ src/addDogToDayCare.ts
@@ -3,6 +3,9 @@ import { Dog } from "./createNewDog"
 let daycareArray: Dog[] = []
 
 let addDogToDayCare = (dog: Dog) => {
+    if (daycareArray.length === 10) {
+        return 'The maximum number of dogs has been reached'
+    }
     daycareArray.push(dog)
     return daycareArray
 }
--- test/addDogToDayCare.test.js
+++ test/addDogToDayCare.test.js
@@ -6,3 +6,18 @@ test('addDogToDayCare adds given Dog argument to array', () => {
 
     expect(addDogToDayCare(dog)).toEqual([{ name: 'Piper'}])
 }) 
+
+test('daycareArray cannot contain more than 10 elements', () => {
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+    addDogToDayCare({ name: 'Piper'})
+
+    expect(addDogToDayCare({ name: 'Piper'})).toEqual('The maximum number of dogs has been reached')
+})
